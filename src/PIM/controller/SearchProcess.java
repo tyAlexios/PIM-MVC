242df8b92ff1937b50bedcb79c5eef363a578bfb
tyAlexios <97611066+tyAlexios@users.noreply.github.com>
2023-11-24 12:01:15 +0800

    push
    push

--- src/PIM/controller/SearchProcess.java
+++ src/PIM/controller/SearchProcess.java
@@ -1,6 +1,8 @@
 package PIM.controller;
 
 import PIM.model.SearchAPI;
+import PIM.view.SearchView;
+import PIM.view.SearchView.*;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -8,7 +10,6 @@ import java.util.Set;
 
 public class SearchProcess implements OperationProcess
 {
-    private Set<String> searchKeySet;
     @Override
     public int verify(String[] cmd)
     {
@@ -23,16 +24,15 @@ public class SearchProcess implements OperationProcess
         SearchAPI searchAPI = new SearchAPI();
         searchAPI.init(null);
         searchAPI.exe(Arrays.copyOfRange(cmd, 1, cmd.length));
-        searchKeySet = searchAPI.getRestKeySet();
-        for (String e : searchKeySet)
+        Set<String> searchKeySet = searchAPI.getRestKeySet();
+        if (searchKeySet.isEmpty())
+            PIM.output(SearchView.getView(ViewPage.SearchNone));
+        else
         {
-            System.out.println(e);
+            PIM.output(SearchView.getView(ViewPage.SearchTitle));
+            for (String key : searchKeySet)
+                PIM.output(String.format("%s\n", key));
         }
-    }
-
-    public Set<String> getSearchKeySet()
-    {
-        return new HashSet<>(searchKeySet);
     }
 
 }
